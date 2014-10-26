526506556d4cc036ab8abcd30a49bf88f2dbce4b
Ryan Barnett <ryan@barnett.io>
2014-10-26 18:00:38 -0400

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,8 +1,8 @@
 __author__ = 'Ryan'
 import os
 def rename():
-    file_list = os.listdir(r'D:\Dev\Projects\CheckIO\prank')
-    os.chdir(r'D:\Dev\Projects\CheckIO\prank')
+    file_list = os.listdir(r'D:\Dev\Projects\CheckIO\prk')
+    os.chdir(r'D:\Dev\Projects\CheckIO\prk')
     for file_name in file_list:
         os.rename(file_name, file_name.translate(None, '0123456789'))
 
